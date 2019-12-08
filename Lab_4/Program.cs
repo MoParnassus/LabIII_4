056a7de0d50a4d10aa66b1356fbe85eb5babc849
MoParnassus <56222484+MoParnassus@users.noreply.github.com>
2019-12-08 17:11:59 +0100

    Signed-off-by: MoParnassus <56222484+MoParnassus@users.noreply.github.com>

---
+++ Lab_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static int[] Kopiuj(int[] tablica) 
+        {
+            int[] nowa = new int[tablica.Length];
+            for (int i = 0; i < nowa.Length; i++)
+            {
+                nowa[i] = tablica[i];
+                Console.WriteLine();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int[][] z = new int[5][];
+            for (int i = 0; i < z.Length; i++)
+            {
+                z[i] = new int[i + 1];
+                for (int i = 0; i < z.Length; i++)
+                {
+                    z[i][j] = i + j;
+                }
+            }
+
+            for (int i = 0; i < Z.Length; i++)
+            {
+                int skopiowana = Kopiuj(z);
+            }
+            
+        }
+    }
+}
